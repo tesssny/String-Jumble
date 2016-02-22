b698861b7e10814831b9789628a146a3d23dec15
tesssny <teresasnyder@hanovernorwichschools.org>
2016-02-22 14:40:38 -0500

    Updated from Brython Server: 2/22/2016 2:40:38 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,6 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 text=input("Please enter a string of text (the bigger the better): ")
+print('You entered "'text'". Now jumble it:')
 l=int(len(text))
 
 for x in range(-1,-l-1,-1):
