250e8bbe0dc4614624e3b1db22ffe7ed21b5a874
tesssny <teresasnyder@hanovernorwichschools.org>
2016-02-22 14:24:42 -0500

    Updated from Brython Server: 2/22/2016 2:24:42 PM

--- stringjumble.py
+++ stringjumble.py
@@ -41,10 +41,10 @@ for w in f:
 
 print("\n")
 
-for x in f:
+for w in words:
     g=0
-    while g < len(x):
-        e=str(x[len(x)-g-1])
+    while g < len(w):
+        e=str(w[len(w)-g-1])
         print("{0}".format(e), end="")
         g=g+1
     print(" ", end="")
